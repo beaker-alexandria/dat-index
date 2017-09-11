a835ac6817ba4b9b0882fa63acd735bd951e6a76
soyuka <soyuka@gmail.com>
2017-09-11 23:20:40 +0200

    add peer count

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function elementView (link, emit) {
         <a href="#" onclick="${expand}">${link.title}</a>
       </div>
       <div class="col-tenth">
-        ${prettyBytes(link.size)} in ${link.files.length} files
+        ${prettyBytes(link.size)} in ${link.files.length} files (${link.peers} peers)
       </div>
       <div class="col-fifth">
         <a href="${link.url}" target="_blank">directory icon</a>
